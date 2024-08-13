b43d59c0cd0c806de995d3b8d3f053703f992074
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2024-08-13 22:35:36 +0530

    Update abc.spec.js

--- abc.spec.js
+++ abc.spec.js
@@ -1,1 +1,6 @@
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
