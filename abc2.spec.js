24baf32899e48a0ab5fb21bcdd88142d04d61288
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2024-08-13 22:53:00 +0530

    Update and rename abc2.spec.js to abc3.spec.js

--- abc2.spec.js
+++
@@ -1,6 +0,0 @@
-test('has title', async ( page ) => {
-  await page.goto('file://' + __dirname + '/index.html');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/My Website/);
-});
