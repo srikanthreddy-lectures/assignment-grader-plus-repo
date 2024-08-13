59a0ed7c376052c18d2dbc00f296cf5a02763acc
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2024-08-13 23:43:21 +0530

    Update and rename abc8.spec.js to abc9.spec.js

--- abc9.spec.js
+++ abc9.spec.js
@@ -1,10 +1,10 @@
-test('has title', async () => {
+test('has title', async ({ page, loadHtml, expect }) => {
     await loadHtml('index.html');
     const title = await page.title();
     await expect(title).toContain('My Website');
 });
 
-test('has heading', async () => {
+test('has heading', async ({ page, loadHtml, expect }) => {
     await loadHtml('index.html');
     const headingText = await page.locator('h1').first().textContent();
     await expect(headingText).toContain('Welcome');
