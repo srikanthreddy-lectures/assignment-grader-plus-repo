018204150cae73a0c074520647de788771a2ad0b
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2024-08-14 14:09:54 +0530

    Update abc9.spec.js

--- abc9.spec.js
+++ abc9.spec.js
@@ -1,7 +1,7 @@
 test('has title', async ({ page, url, expect }) => {
     await page.goto(url);
     const title = await page.title();
-    await expect(title).toContain('Hello');
+    await expect(title).toContain('ACCCCCC');
 });
 
 test('has heading', async ({ page, url, expect }) => {
