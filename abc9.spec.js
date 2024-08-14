87b0df16e2778b6383540abd6d7053f90c32ce21
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2024-08-14 07:46:14 +0530

    Update abc9.spec.js

--- abc9.spec.js
+++ abc9.spec.js
@@ -1,11 +1,9 @@
-test('has title', async ({ page, loadHtml, expect }) => {
-    await loadHtml('index.html');
+test('has title', async ({ page, expect }) => {
     const title = await page.title();
     await expect(title).toContain('My Website');
 });
 
-test('has heading', async ({ page, loadHtml, expect }) => {
-    await loadHtml('index.html');
+test('has heading', async ({ page, expect }) => {
     const headingText = await page.locator('h1').first().textContent();
     await expect(headingText).toContain('Welcome');
 });
